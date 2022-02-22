f7f4f57c4ae08bf098783c8f904337e7261b8359
ujin_bang <qkddnwls14@gmail.com>
2022-02-22 11:35:09 +0900

    리스트뷰의 실제 데이터 적용 중  - 가게 이름만 우선 적용

--- app/src/main/java/com/example/otherapp_20220222/adapters/StoreAdapter.kt
+++ app/src/main/java/com/example/otherapp_20220222/adapters/StoreAdapter.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.otherapp_20220222.R
 import com.example.otherapp_20220222.datas.StoreData
+import com.willy.ratingbar.ScaleRatingBar
 
 class StoreAdapter(
     val mContext: Context,
@@ -23,6 +26,14 @@ class StoreAdapter(
             tempRow = LayoutInflater.from(mContext).inflate(R.layout.store_list_item, null)
         }
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val imgLogo = row.findViewById<ImageView>(R.id.imgLogo)
+        val txtStoreName =row.findViewById<TextView>(R.id.txtStoreName)
+        val ratingBar = row.findViewById<ScaleRatingBar>(R.id.ratingBar)
+
+        txtStoreName.text = data.name
         return row
     }
 }
