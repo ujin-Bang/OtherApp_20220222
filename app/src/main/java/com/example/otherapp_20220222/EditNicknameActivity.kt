26034f6e92d10921afc1b4b2e91c983f3f70fe95
ujin_bang <qkddnwls14@gmail.com>
2022-02-22 15:45:34 +0900

    닉네임 변경 화면 추가만

---
+++ app/src/main/java/com/example/otherapp_20220222/EditNicknameActivity.kt
@@ -0,0 +1,18 @@
+package com.example.otherapp_20220222
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){
+
+    }
+    fun setValues(){}
+}
