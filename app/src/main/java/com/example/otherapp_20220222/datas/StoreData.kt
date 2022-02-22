e197b74dc58b7b0f855189ba67b2afbd4b55f584
ujin_bang <qkddnwls14@gmail.com>
2022-02-22 14:42:05 +0900

    Serializable을 활용해서 클릭된 방을 첨부 > 화면이동

--- app/src/main/java/com/example/otherapp_20220222/datas/StoreData.kt
+++ app/src/main/java/com/example/otherapp_20220222/datas/StoreData.kt
@@ -1,9 +1,11 @@
 package com.example.otherapp_20220222.datas
 
+import java.io.Serializable
+
 class StoreData(
     val name: String,
     val rating: Double,
     val phonNum: String,
     val logoImageUrl : String,
-) {
+): Serializable {
 }
