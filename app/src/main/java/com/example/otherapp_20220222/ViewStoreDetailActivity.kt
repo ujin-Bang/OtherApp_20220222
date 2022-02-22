fe06a193374527cfbf532ecd9f0522c734ddf00e
ujin_bang <qkddnwls14@gmail.com>
2022-02-22 15:20:48 +0900

    전홯 주문 버튼 클릭 > 권한 물어보기 기능만

--- app/src/main/java/com/example/otherapp_20220222/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/example/otherapp_20220222/ViewStoreDetailActivity.kt
@@ -1,9 +1,13 @@
 package com.example.otherapp_20220222
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.otherapp_20220222.datas.StoreData
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : AppCompatActivity() {
@@ -20,6 +24,30 @@ class ViewStoreDetailActivity : AppCompatActivity() {
 
     fun setupEvants(){
 
+        val pl = object : PermissionListener {
+            override fun onPermissionGranted() {
+
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                Toast.makeText(this@ViewStoreDetailActivity, "통화 권한이 없습니다.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+
+        }
+
+       btnCall.setOnClickListener {
+
+        TedPermission.create()
+            .setPermissionListener(pl)
+            .setPermissions(Manifest.permission.CALL_PHONE)
+            .check()
+
+       }
+
+    }
+    fun setValues(){
         Glide.with(this).load(mStoreData.logoImageUrl).into(imgLogo)
 
         txtStoreName.text = mStoreData.name
@@ -29,9 +57,6 @@ class ViewStoreDetailActivity : AppCompatActivity() {
         ratingBar.rating = mStoreData.rating.toFloat()
 
         txtRating.text = "(${mStoreData.rating})"
-
-    }
-    fun setValues(){
 
     }
 }
