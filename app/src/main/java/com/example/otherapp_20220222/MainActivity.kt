9fb79afa78485390c27525f173d89caaab890480
ujin_bang <qkddnwls14@gmail.com>
2022-02-22 10:04:58 +0900

    뷰페이저 어댑터/ 뷰페이저 연결 작업  - 실행 결과로도 화면 이동 가능

--- app/src/main/java/com/example/otherapp_20220222/MainActivity.kt
+++ app/src/main/java/com/example/otherapp_20220222/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.otherapp_20220222
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.otherapp_20220222.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdapter: MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setValues()
+        setupEvents()
     }
+
+    fun setupEvents(){
+
+    }
+
+    fun setValues(){
+
+        mAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mAdapter
+
+    }
+
+
 }
