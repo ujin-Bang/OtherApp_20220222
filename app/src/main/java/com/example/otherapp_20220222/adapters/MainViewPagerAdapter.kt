0d7aa0f44bd008fb29c40ea0a0760a9c9048c8c3
ujin_bang <qkddnwls14@gmail.com>
2022-02-22 09:54:54 +0900

    뷰페이저 어댑터 클래스 제작중 - 상속 부분 해결

---
+++ app/src/main/java/com/example/otherapp_20220222/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.otherapp_20220222.adapters
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
+}
